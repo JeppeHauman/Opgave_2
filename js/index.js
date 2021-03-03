1ef196e49397fed3c547a4b4f6ee3ab2c3997b80
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-03 10:50:41 +0100

    Made the thing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 Vue.createApp({
-    data
-})
+    data() {
+        return {
+            input: null,
+            output: "null",
+
+        }
+    },
+    methods: {
+        Add() {
+            this.output = this.input
+        }
+    },
+    computed: {
+        Upper() {
+            return this.output.toUpperCase()
+        },
+        Lower() {
+            return this.output.toLowerCase()
+        }
+    }
+}).mount("#app")
